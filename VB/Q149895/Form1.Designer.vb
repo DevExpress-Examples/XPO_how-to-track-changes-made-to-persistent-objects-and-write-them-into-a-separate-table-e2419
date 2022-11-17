69ddfd6a3d1e030349df9183a4d0d101ab6e9fb3
DevExpressExampleBot <fakeEmail>
2022-11-17 09:42:00 +0400

    autogenerate VB

--- VB/Q149895/Form1.Designer.vb
+++ VB/Q149895/Form1.Designer.vb
@@ -1,116 +1,120 @@
-﻿Namespace Q149895
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
+Namespace Q149895
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class Form1
 
-		#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Me.gridControl1 = New DevExpress.XtraGrid.GridControl()
-			Me.xpCollection1 = New DevExpress.Xpo.XPCollection()
-			Me.gridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
-			Me.colCategoryID = New DevExpress.XtraGrid.Columns.GridColumn()
-			Me.colCategoryName = New DevExpress.XtraGrid.Columns.GridColumn()
-			Me.colDescription = New DevExpress.XtraGrid.Columns.GridColumn()
-			Me.session1 = New DevExpress.Xpo.Session()
-			CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.xpCollection1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.gridView1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.session1, System.ComponentModel.ISupportInitialize).BeginInit()
-			Me.SuspendLayout()
-			' 
-			' gridControl1
-			' 
-			Me.gridControl1.DataSource = Me.xpCollection1
-			Me.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill
-'INSTANT VB NOTE: The following InitializeComponent event wireup was converted to a 'Handles' clause:
-'ORIGINAL LINE: this.gridControl1.EmbeddedNavigator.ButtonClick += new DevExpress.XtraEditors.NavigatorButtonClickEventHandler(this.OnGridControlEmbeddedNavigatorButtonClick);
-			Me.gridControl1.Location = New System.Drawing.Point(0, 0)
-			Me.gridControl1.MainView = Me.gridView1
-			Me.gridControl1.Name = "gridControl1"
-			Me.gridControl1.Size = New System.Drawing.Size(644, 427)
-			Me.gridControl1.TabIndex = 0
-			Me.gridControl1.UseEmbeddedNavigator = True
-			Me.gridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() { Me.gridView1})
-			' 
-			' xpCollection1
-			' 
-			Me.xpCollection1.DisplayableProperties = "This;CategoryID;CategoryName;Description"
-			Me.xpCollection1.ObjectType = GetType(Northwind.Categories)
-			Me.xpCollection1.Session = Me.session1
-			' 
-			' gridView1
-			' 
-			Me.gridView1.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() { Me.colCategoryID, Me.colCategoryName, Me.colDescription})
-			Me.gridView1.GridControl = Me.gridControl1
-			Me.gridView1.Name = "gridView1"
-			' 
-			' colCategoryID
-			' 
-			Me.colCategoryID.Caption = "CategoryID"
-			Me.colCategoryID.FieldName = "CategoryID"
-			Me.colCategoryID.Name = "colCategoryID"
-			Me.colCategoryID.Visible = True
-			Me.colCategoryID.VisibleIndex = 0
-			' 
-			' colCategoryName
-			' 
-			Me.colCategoryName.Caption = "CategoryName"
-			Me.colCategoryName.FieldName = "CategoryName"
-			Me.colCategoryName.Name = "colCategoryName"
-			Me.colCategoryName.Visible = True
-			Me.colCategoryName.VisibleIndex = 1
-			' 
-			' colDescription
-			' 
-			Me.colDescription.Caption = "Description"
-			Me.colDescription.FieldName = "Description"
-			Me.colDescription.Name = "colDescription"
-			Me.colDescription.Visible = True
-			Me.colDescription.VisibleIndex = 2
-			' 
-			' Form1
-			' 
-			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
-			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-			Me.ClientSize = New System.Drawing.Size(644, 427)
-			Me.Controls.Add(Me.gridControl1)
-			Me.Name = "Form1"
-			Me.Text = "Form1"
-			CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.xpCollection1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.gridView1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.session1, System.ComponentModel.ISupportInitialize).EndInit()
-			Me.ResumeLayout(False)
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
 
-		End Sub
+            MyBase.Dispose(disposing)
+        End Sub
 
-		#End Region
+#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.gridControl1 = New DevExpress.XtraGrid.GridControl()
+            Me.xpCollection1 = New DevExpress.Xpo.XPCollection()
+            Me.gridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
+            Me.colCategoryID = New DevExpress.XtraGrid.Columns.GridColumn()
+            Me.colCategoryName = New DevExpress.XtraGrid.Columns.GridColumn()
+            Me.colDescription = New DevExpress.XtraGrid.Columns.GridColumn()
+            Me.session1 = New DevExpress.Xpo.Session()
+            CType((Me.gridControl1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.xpCollection1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.gridView1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.session1), System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' gridControl1
+            ' 
+            Me.gridControl1.DataSource = Me.xpCollection1
+            Me.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill
+            AddHandler Me.gridControl1.EmbeddedNavigator.ButtonClick, New DevExpress.XtraEditors.NavigatorButtonClickEventHandler(AddressOf Me.OnGridControlEmbeddedNavigatorButtonClick)
+            Me.gridControl1.Location = New System.Drawing.Point(0, 0)
+            Me.gridControl1.MainView = Me.gridView1
+            Me.gridControl1.Name = "gridControl1"
+            Me.gridControl1.Size = New System.Drawing.Size(644, 427)
+            Me.gridControl1.TabIndex = 0
+            Me.gridControl1.UseEmbeddedNavigator = True
+            Me.gridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.gridView1})
+            ' 
+            ' xpCollection1
+            ' 
+            Me.xpCollection1.DisplayableProperties = "This;CategoryID;CategoryName;Description"
+            Me.xpCollection1.ObjectType = GetType(Northwind.Categories)
+            Me.xpCollection1.Session = Me.session1
+            ' 
+            ' gridView1
+            ' 
+            Me.gridView1.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() {Me.colCategoryID, Me.colCategoryName, Me.colDescription})
+            Me.gridView1.GridControl = Me.gridControl1
+            Me.gridView1.Name = "gridView1"
+            ' 
+            ' colCategoryID
+            ' 
+            Me.colCategoryID.Caption = "CategoryID"
+            Me.colCategoryID.FieldName = "CategoryID"
+            Me.colCategoryID.Name = "colCategoryID"
+            Me.colCategoryID.Visible = True
+            Me.colCategoryID.VisibleIndex = 0
+            ' 
+            ' colCategoryName
+            ' 
+            Me.colCategoryName.Caption = "CategoryName"
+            Me.colCategoryName.FieldName = "CategoryName"
+            Me.colCategoryName.Name = "colCategoryName"
+            Me.colCategoryName.Visible = True
+            Me.colCategoryName.VisibleIndex = 1
+            ' 
+            ' colDescription
+            ' 
+            Me.colDescription.Caption = "Description"
+            Me.colDescription.FieldName = "Description"
+            Me.colDescription.Name = "colDescription"
+            Me.colDescription.Visible = True
+            Me.colDescription.VisibleIndex = 2
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(644, 427)
+            Me.Controls.Add(Me.gridControl1)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            CType((Me.gridControl1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.xpCollection1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.gridView1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.session1), System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+        End Sub
 
-		Private WithEvents gridControl1 As DevExpress.XtraGrid.GridControl
-		Private gridView1 As DevExpress.XtraGrid.Views.Grid.GridView
-		Private xpCollection1 As DevExpress.Xpo.XPCollection
-		Private colCategoryID As DevExpress.XtraGrid.Columns.GridColumn
-		Private colCategoryName As DevExpress.XtraGrid.Columns.GridColumn
-		Private colDescription As DevExpress.XtraGrid.Columns.GridColumn
-		Private session1 As DevExpress.Xpo.Session
-	End Class
+#End Region
+        Private gridControl1 As DevExpress.XtraGrid.GridControl
+
+        Private gridView1 As DevExpress.XtraGrid.Views.Grid.GridView
+
+        Private xpCollection1 As DevExpress.Xpo.XPCollection
+
+        Private colCategoryID As DevExpress.XtraGrid.Columns.GridColumn
+
+        Private colCategoryName As DevExpress.XtraGrid.Columns.GridColumn
+
+        Private colDescription As DevExpress.XtraGrid.Columns.GridColumn
+
+        Private session1 As DevExpress.Xpo.Session
+    End Class
 End Namespace
-
--- VB/Q149895/Program.vb
+++ VB/Q149895/Program.vb
@@ -1,20 +1,20 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Windows.Forms
+Imports System
 Imports DevExpress.Xpo
 Imports DevExpress.Xpo.DB
 
 Namespace Q149895
-	Friend Module Program
-		''' <summary>
-		''' The main entry point for the application.
-		''' </summary>
-		<STAThread>
-		Sub Main()
-			Application.EnableVisualStyles()
-			Application.SetCompatibleTextRenderingDefault(False)
-			XpoDefault.ConnectionString = SQLiteConnectionProvider.GetConnectionString("nwind.sqlite")
-			Application.Run(New Form1())
-		End Sub
-	End Module
+
+    Friend Module Program
+
+        ''' <summary>
+        ''' The main entry point for the application.
+        ''' </summary>
+        <STAThread>
+        Sub Main()
+            Application.EnableVisualStyles()
+            Application.SetCompatibleTextRenderingDefault(False)
+            XpoDefault.ConnectionString = SQLiteConnectionProvider.GetConnectionString("nwind.sqlite")
+            Application.Run(New Form1())
+        End Sub
+    End Module
 End Namespace
